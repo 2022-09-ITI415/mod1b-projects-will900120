d2ad0a11b34872c645a38eda8485f60f60655bbf
will900120 <wm287@scarletmail.rutgers.edu>
2022-10-06 21:52:14 -0400

    live text

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,11 +7,13 @@ using UnityEngine.UI;
 public class PlayerController : MonoBehaviour
 {
     [Header("Set Dynamically")]
+    [Header("Set Dynamically")]
     public float speed = 0;
     private Rigidbody rb;
     private float movementX;
     private float movementY;
     public Text scoreGT;
+    public Text livesGT;
     
     
    
@@ -21,8 +23,11 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         
         GameObject scoreGO = GameObject.Find("CountText");
+        GameObject liveGO = GameObject.Find("Lives");
         scoreGT = scoreGO.GetComponent<Text>();
+        livesGT = liveGO.GetComponent<Text>();
         scoreGT.text = "0";
+        livesGT.text = "5";
     }
 
     void OnMove(InputValue movementValue){
@@ -58,6 +63,11 @@ public class PlayerController : MonoBehaviour
         GameObject collidedWith = coll.gameObject;
         if(collidedWith.tag == "Apple"){
             Destroy(collidedWith);
+
+            int lives = int.Parse( livesGT.text );
+            lives -= 1;
+            livesGT.text = lives.ToString();
+            
         }
     }
 }
